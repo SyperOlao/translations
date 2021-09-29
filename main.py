cc4b96f4d36f4ee9604f04594e678b643f78c0c9
SyperOlao <olay1234538@gmail.com>
2021-09-30 01:31:55 +0300

    Доработана программа

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
 import math 
+import time
 
-def two_to_ten(num,system_num=2):
-    remainder = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15}
+def base_to_ten(num, system_base=2):
+    remainder = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15}
     num = str(num)
+    sign = 1
+    if num.find('-') >= 0:
+        sign = -1
+        num = num.replace('-', '')
     power = num.find('.') - 1 if '.' in num else len(num) - 1
     num = num.replace('.', '')
     result = 0
     for i in range(len(num)):
-        result += remainder[num[i]] * system_num ** power
+        result += remainder[num[i]] * system_base ** power
         power -= 1
-    return result
+    return result * sign
 
-def ten_to_system(num, system_num=2):
+def ten_to_base(num, system_base=2):
     remainder = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
-    whole = int(num)
-    fractional = float(num) % 1
-    result = "." 
-    while whole >= system_num:
-        result += str(remainder[whole % system_num])
-        whole //= system_num
-    result += str(remainder[whole % system_num])
-    result = result[::-1] 
+    whole = int(abs(num))
+    fractional = float(abs(num)) % 1
+    result = "." if fractional != 0 else "" 
+    while whole >= system_base:
+        result += str(remainder[whole % system_base])
+        whole //= system_base
+    result += str(remainder[whole % system_base])
+    result = ('' if num >= 0 else '-') + result[::-1] 
     while fractional > 0:  
-        temp_fractional, temp_whole= math.modf(fractional * system_num)
-        fractional = temp_fractional
+        temp_fractional, temp_whole = math.modf(fractional * system_base)
+        fractional = abs(temp_fractional)
         result += str(remainder[int(temp_whole)])
     return result
 
 
+def translation(num, in_base, out_base):
+    return ten_to_base(base_to_ten(num, in_base), out_base)
 
-num = 6578.7567
-print(f"num: {num},\nresult 2 in 10: {ten_to_system(num)}, \
-\nresult 10 in 2: {two_to_ten(ten_to_system(num, 16), 16)}, \
-\nresult 10 in 16: {ten_to_system(num, 16)}")
+
+def result_data(text_url):
+    with open(text_url) as f:
+        num = f.readlines()
+    start_time = time.time()
+    for i in range(len(num)):
+        num[i] = num[i].replace('\n', '')
+        num[i] = translation(num[i], 10, 2)
+        num[i] = translation(num[i], 2, 16)
+        num[i] = float(translation(num[i], 16, 10))
+    end_time = time.time() - start_time
+    result_sum = sum(num)
+    return end_time, result_sum
+
+
+def result_time_sum(text):
+    result_time, result_sum = result_data(text)
+    print("data: ", text)
+    print(f'Сумма всех десятичных чисел после завершения цикла преобразований: {result_sum}')
+    print(f'Время программы в миллисекундах:  {result_time*1000}ms')
+
+text1 = "data10.txt"
+text2 = "data11.txt"
+
+result_time_sum(text1)
+result_time_sum(text2)
+
